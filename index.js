e998827a90b6b8cc31658bdc6cee1795b44135a3
DILSHAD ALAM <rayeendilshan@gmail.com>
2022-09-10 12:17:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT  = process.env.PORT | 3001;
+const app = express();
+
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log('Connected at port  $PORT')
+})
